3049905429530c265ec3ab8e496a2e12a04eb1d4
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2026-04-19 08:01:03 -0500

    Update sol.py

--- possible_combos/sol.py
+++ possible_combos/sol.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.combos = []
@@ -10,7 +12,7 @@ class Solution:
         candidates: List[int], 
         target: int, 
         curr_sum: int, 
-        curr_combos: Tuple[int]
+        curr_combos: List[int]
     ) -> None:
         if curr_sum > target:
             return 
@@ -24,3 +26,17 @@ class Solution:
                 curr_sum + candidates[i], 
                 curr_combos + [candidates[i]]
             )
+
+sol = Solution() 
+
+test_cases = [
+    ([2,3,6,7], 10, [[2,2,2,2,2],[2,2,3,3],[2,2,6],[3,7]])
+]
+
+for test_case in test_cases: 
+    print(test_case[0])
+    print(test_case[1])
+    print(test_case[2])
+
+    if list(sorted(sol.combinationSum(test_case[0], test_case[1]))) != list(sorted(test_case[2])):
+        raise
